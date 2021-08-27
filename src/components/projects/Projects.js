606d5b795b5eb683b541656f055399e3b92bb294
hexaquarks <seseriusca@gmail.com>
2021-08-27 18:28:04 -0400

    Progress on the Projects layout

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -3,9 +3,19 @@ import styles from '../projects/Projects.module.css';
 const Projects = () => {
     return (
         <div className={styles.container}>
-            
+            <div className={styles.firstProjectGif}>
+                <div className={styles.firstProjectDescription}>
+
+                </div>
+            </div>
+
+            <div className={styles.secondProjectGif}>
+                <div className={styles.secondProjectDescription}>
+
+                </div>
+            </div>
         </div>
     );
-}  
+}
 
 export default Projects;
