4470b6b0c02041c4f5e80e348a03fdd82e92077f
hexaquarks <seseriusca@gmail.com>
2021-08-28 20:21:51 -0400

    Added some color hover stylings and begun the animation on the cover name title

--- src/components/cover/Cover.js
+++ src/components/cover/Cover.js
@@ -5,9 +5,27 @@ const Cover = () => {
     return (
         <div className={styles.container} >
             <div className={styles.description} >
-                <h1>
+                <ul>
+                    <li>M</li>
+                    <li>i</li>
+                    <li>h</li>
+                    <li>a</li>
+                    <li>i</li>
+                    <li>l</li>
+                    <li>&nbsp;</li>
+                    <li>A</li>
+                    <li>n</li>
+                    <li>g</li>
+                    <li>h</li>
+                    <li>e</li>
+                    <li>l</li>
+                    <li>i</li>
+                    <li>c</li>
+                    <li>i</li>
+                </ul>
+                {/* <h1>
                     Mihail Anghelici
-                </h1> 
+                </h1>  */}
                 <h2>
                     Prospecting software developper living in the Greater Montreal area. I am interested in most technologies across the back-end and front-end, and the respective practices associated with them. 
                 </h2>
