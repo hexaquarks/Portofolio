5ba34234d5c7a49576ba5d174f34a0c74a071cbb
hexaquarks <seseriusca@gmail.com>
2021-08-29 20:12:03 -0400

    Working education drop down information

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -5,13 +5,26 @@ import downArrow from '../../assets/down_arrow.png';
 import { useState, useRef } from 'react';
 import { CSSTransition } from 'react-transition-group';
 import Fade from '@material-ui/core/Fade';
+import { TextField } from '@material-ui/core';
 
 const About = () => {
     const [topDropDown, setTopDropDown] = useState(false);
+    const [midDropDown, setMidDropDown] = useState(false);
+    const [botDropDown, setBotDropDown] = useState(false);
 
     const onClick = (type) => {
-        if(type==='top'){
+        if (type === 'top') {
             setTopDropDown(!topDropDown);
+            setBotDropDown(false);
+            setMidDropDown(false);
+        } else if (type === 'mid') {
+            setMidDropDown(!midDropDown);
+            setTopDropDown(false);
+            setBotDropDown(false);
+        } else if (type === 'bot') { 
+            setBotDropDown(!botDropDown);
+            setTopDropDown(false);
+            setMidDropDown(false);
         }
     }
     return (
@@ -41,16 +54,27 @@ const About = () => {
                 <hr />
                 <p>
                     <div className={styles.dropDownArrow}>
-                        <img className={topDropDown===true ? `${styles.dropArrowTop} ${styles.doRotate}` : `${styles.dropArrowTop}`} 
-                            src={downArrow} width="30" height="30" 
+                        <img className={topDropDown === true ? `${styles.dropArrow} ${styles.doRotate}` : `${styles.dropArrow}`}
+                            src={downArrow} width="30" height="30"
                             onClick={() => { onClick('top') }}
-                            // styles={{Animation: `rotate 1s`}}
-                            >
+                            styles={{ Animation: `rotate 1s` }}
+                        >
                         </img>
-                        {topDropDown &&  <Fade in={true}><div className={styles.topPanel}>
-                            test
-
-                        </div></Fade>}
+                        <Fade in={topDropDown}>
+                            <div className={styles.decriptionPanel}>
+                                <fieldset>
+                                    <legend>Relevent Coursework</legend>
+                                    <div className={styles.coursesList} >
+                                        <ul>
+                                            <li>Introduction to Computer Science</li>
+                                            <li>Software Systems</li>
+                                            <li>Matrix Numerical analysis</li>
+                                            <li>Discrete Mathematics</li>
+                                        </ul>
+                                    </div>
+                                </fieldset>
+                            </div>
+                        </Fade>
                     </div>
                     <div>
                         <img src={mcgillIcon} width="50" height="50" alt="mcgillIcon" />
@@ -60,7 +84,26 @@ const About = () => {
                     </div>
 
                     <div className={styles.dropDownArrow}>
-                        <img src={downArrow} width="30" height="30" />
+                        <img className={midDropDown === true ? `${styles.dropArrow} ${styles.doRotate}` : `${styles.dropArrow}`}
+                            src={downArrow} width="30" height="30"
+                            onClick={() => { onClick('mid') }}>
+                        </img>
+                        <Fade in={midDropDown}>
+                            <div className={styles.decriptionPanel}>
+                                <fieldset>
+                                    <legend>Relevent Coursework</legend>
+                                    <div className={styles.coursesList} >
+                                        <ul>
+                                            <li>Quantum Mechanics I (Hons.)</li>
+                                            <li>Electricy and Magnetism I (Hons.)</li>
+                                            <li>Ordinary Differential Equations (Hons.)</li>
+                                            <li>Partial Differential Equations (Hons.)</li>
+                                            <li>Signals Processing</li>
+                                        </ul>
+                                    </div>
+                                </fieldset>
+                            </div>
+                        </Fade>
                     </div>
                     <div>
                         <img src={mcgillIcon} width="50" height="50" alt="mcgillIcon" />
@@ -70,7 +113,27 @@ const About = () => {
                     </div>
 
                     <div className={styles.dropDownArrow}>
-                        <img src={downArrow} width="30" height="30" />
+                        <img className={botDropDown === true 
+                                ? `${styles.dropArrow} ${styles.doRotate}` 
+                                : `${styles.dropArrow}`}
+                            src={downArrow} width="30" height="30"
+                            onClick={() => { onClick('bot') }}>
+                        </img>
+                        <Fade in={botDropDown}>
+                            <div className={styles.decriptionPanel}>
+                                <fieldset>
+                                    <legend>Relevent Coursework</legend>
+                                    <div className={styles.coursesList} >
+                                        <ul>
+                                            <li>Introduction to Programming</li>
+                                            <li>Data Structures and OOP.</li>
+                                            <li>Programming with Graphical Interfaces</li>
+                                            <li>Degree Programming Final Project </li>
+                                        </ul>
+                                    </div>
+                                </fieldset>
+                            </div>
+                        </Fade>
                     </div>
                     <div>
                         <img src={bdebIcon} width="40" height="40" style={{ marginLeft: `5px` }} alt="bdebIcon" />
