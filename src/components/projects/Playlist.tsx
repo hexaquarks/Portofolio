135889fa461ee45307913c7251f250c0cd3cb138
hexaquarks <seseriusca@gmail.com>
2021-09-02 09:26:38 -0400

    Finally working API fetching. Started playlist SCSS

--- src/components/projects/Playlist.tsx
+++ src/components/projects/Playlist.tsx
@@ -1,61 +1,29 @@
 import { useEffect } from 'react';
-var SC = require('soundcloud');
+const styles = require('../projects/Playlist.module.scss');
 
-// const useScript = url => {
-//     useEffect(() => {
-//         const script = document.createElement('script');
+const SC = require('soundcloud');
+SC.initialize({
+  client_id: '1dff55bf515582dc759594dac5ba46e9'
+});
 
-//         script.src = url;
-//         script.async = true;
-
-//         document.body.appendChild(script);
-//         window.sc
-
-//         return () => {
-//             document.body.removeChild(script);
-//         }
-//     }, [url]);
-// };
-
+const player = (trackId) => {
+  SC.stream('/tracks/' + trackId).then(function(player){
+    player.setVolume(0.0);
+    console.log(player);
+    player.play().then(function(){
+      console.log('Playback started!');
+    }).catch(function(e){
+      console.error('Playback rejected. Try calling play() from a user interaction.', e);
+    });
+  });
+}  
 const Playlist = () => {
-  
-  // SC.initialize({
-  //   client_id: 'APKAI6TU7MMXM5DG6EPQ'
-  // });
-  // /*.header("Access-Control-Allow-Origin", "*") */
-  // SC.get('/user/181/tracks').then(function(tracks){
-  //   alert('Latest track: ' + tracks[0].title);
-  // });
-
-  fetch('https://api.soundcloud.com', {
-    method: "GET",
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": "true",
-      "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
-      "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    }
-  })
-    .then(results => results.json())
-    .then(info => {
-      console.log(info);
-    })
-
-  // stream track id 293
-  //   SC.stream('/tracks/293').then(function(player){
-  //     player.play().then(function(){
-  //       console.log('Playback started!');
-  //     }).catch(function(e){
-  //       console.error('Playback rejected. Try calling play() from a user interaction.', e);
-  //     });
-  //   });
-  // useScript('https://connect.soundcloud.com/sdk/sdk-3.3.2.js');
-
   return (
     <div>
       {/* Hello world { externalFunction() } */}
     </div>
   )
 }
+
 
 export default Playlist;
