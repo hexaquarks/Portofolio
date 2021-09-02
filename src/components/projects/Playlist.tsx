2c42fc55a0cbdc862deab0c327df9eb46b7aa93c
hexaquarks <seseriusca@gmail.com>
2021-09-02 14:17:19 -0400

    Some progress on the built of playlist

--- src/components/projects/Playlist.tsx
+++ src/components/projects/Playlist.tsx
@@ -1,25 +1,64 @@
-import { useEffect } from 'react';
+import { useState ,useEffect } from 'react';
+import playIcon from '../../assets/play.png';
+import pauseIcon from '../../assets/pause.png';
+import prevIcon from '../../assets/prev.png';
+import nextIcon from '../../assets/next.png';
 const styles = require('../projects/Playlist.module.scss');
+
 
 const SC = require('soundcloud');
 SC.initialize({
   client_id: '1dff55bf515582dc759594dac5ba46e9'
 });
 
+
+
 const player = (trackId) => {
   SC.stream('/tracks/' + trackId).then(function(player){
     player.setVolume(0.0);
     console.log(player);
     player.play().then(function(){
-      console.log('Playback started!');
+
+      // console.log('Playback started!');
+
     }).catch(function(e){
-      console.error('Playback rejected. Try calling play() from a user interaction.', e);
+      console.error('Playback rejected ', e);
     });
   });
 }  
 const Playlist = () => {
+  const [playState, setPlayState ] = useState(false);
+
+  const playClicked = () => {
+    playState === true 
+        ? setPlayState(false) 
+        : setPlayState(true)
+  }
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.picture} />
+        <div className={styles.controls}> 
+          <div className={styles.time}> 
+
+          </div>
+          <div className={styles.actions}> 
+            <div className={styles.prev}> 
+                <img src={prevIcon}/>
+            </div>
+            <div className={styles.playState}>
+                <img src={playState===true 
+                            ? playIcon 
+                            : pauseIcon} 
+                      alt="playStateIcon" />
+            </div>
+            <div className={styles.next}> 
+          
+            </div>
+          
+          </div>
+        </div>  
+
+
       {/* Hello world { externalFunction() } */}
     </div>
   )
