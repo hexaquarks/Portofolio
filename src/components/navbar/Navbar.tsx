33b33230392def14fa99579e5f380c67f47aecac
hexaquarks <seseriusca@gmail.com>
2021-09-11 14:44:23 -0400

    Final minor styling adjustments

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,21 +1,32 @@
 import react from 'react';
 import NavbarComponent from '../navbar/NavbarComponent';
+import linkedinIcon from '../../assets/linkedinIcon.png';
+import githubIcon from '../../assets/githubOrange.png';
 
 const styles = require('../navbar/Navbar.module.scss');
 
 const Navbar = () => {
     const props = [
-        {name: 'Home' , targetName: 'homeDiv'},
-        {name: 'About' , targetName: 'aboutDiv'},
-        {name: 'Projects' , targetName: 'projectsDiv'},
-        {name: 'Extras' , targetName: 'extrasDiv'},
-        {name: 'Contact' , targetName: 'contactDiv'}
+        { name: 'Home', targetName: 'homeDiv' },
+        { name: 'About', targetName: 'aboutDiv' },
+        { name: 'Projects', targetName: 'projectsDiv' },
+        { name: 'Extras', targetName: 'extrasDiv' },
+        { name: 'Contact', targetName: 'contactDiv' }
     ]
 
     return (
         <div className={styles.navbar}>
+            <div className={styles.icons}>
+                <a href="https://www.linkedin.com/in/mihail-anghelici-85196a20a/" target="_blank">
+                    <img src={linkedinIcon} width="30" height="30" />
+                </a>
+                <a href="https://github.com/hexaquarks" target="_blank">
+                    <img src={githubIcon} width="30" height="30"
+                        style={{ marginLeft: '10px' }} />                
+                </a>
+            </div>
             {props.map((value, index) => (
-                <NavbarComponent 
+                <NavbarComponent
                     name={value.name}
                     targetName={value.targetName}
                     key={index} />
